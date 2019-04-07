1c6875634cf842e92e494804783c650be9fdbbf8
Lukas <strandlaefer@googlemail.com>
2019-04-07 18:22:51 +0200

    added working example to call java function inside Unity

---
+++ location-android-plugin/locationlib/src/main/java/com/hfugames/locationlib/LocationManager.java
@@ -0,0 +1,11 @@
+package com.hfugames.locationlib;
+
+import android.util.Log;
+
+public class LocationManager
+{
+    public void LogNativeLogcatMessage()
+    {
+        Log.d("Unity", "Native Logcat Message");
+    }
+}
---
+++ unity-project/Assets/Scripts/LocationManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationManager : MonoBehaviour
+{
+    private AndroidJavaObject javaClass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        javaClass = new AndroidJavaObject("com.hfugames.locationlib.LocationManager");
+        javaClass.Call("LogNativeLogcatMessage");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
