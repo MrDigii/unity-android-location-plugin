f4007a88d356bbb31bbfdb79ee1d022484f4d3d8
Lukas <strandlaefer@googlemail.com>
2019-04-08 21:00:26 +0200

    added functionality to call Unity functions in java plugin

--- location-android-plugin/locationlib/src/main/java/com/hfugames/locationlib/LocationManager.java
+++ location-android-plugin/locationlib/src/main/java/com/hfugames/locationlib/LocationManager.java
@@ -1,11 +1,41 @@
 package com.hfugames.locationlib;
 
 import android.util.Log;
+import com.unity3d.player.UnityPlayer;
 
 public class LocationManager
 {
     public void LogNativeLogcatMessage()
     {
         Log.d("Unity", "Native Logcat Message");
+    }
+
+    public void LogNumberSentFromUnity(int _passedNumber)
+    {
+        Log.d("Unity", "Number passed is: " + _passedNumber);
+    }
+
+    public int AddToNumber(int _number, int _addAmount)
+    {
+        return _number + _addAmount;
+    }
+
+    public void CallAOrB(String _value)
+    {
+        if(_value.equals("A")) {
+            DoSomethingA();
+        } else if (_value.equals("B")) {
+            DoSomethingB();
+        }
+    }
+
+    public void DoSomethingA()
+    {
+        UnityPlayer.UnitySendMessage("LocationManager", "ChangeTextToA", "2");
+    }
+
+    public void DoSomethingB()
+    {
+        UnityPlayer.UnitySendMessage("LocationManager", "ChangeTextToB", "3");
     }
 }
--- unity-project/Assets/Scripts/LocationManager.cs
+++ unity-project/Assets/Scripts/LocationManager.cs
@@ -1,21 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LocationManager : MonoBehaviour
 {
     private AndroidJavaObject javaClass;
+    [SerializeField]
+    private Text numberText;
 
     // Start is called before the first frame update
     void Start()
     {
         javaClass = new AndroidJavaObject("com.hfugames.locationlib.LocationManager");
         javaClass.Call("LogNativeLogcatMessage");
+        javaClass.Call("LogNumberSentFromUnity", 20);
+        numberText.text = javaClass.Call<int>("AddToNumber", 10, 5).ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CallAOrBInJava(string _value)
     {
-        
+        javaClass.Call("CallAOrB", _value);     
+    }
+
+    public void ChangeTextToA(string _value)
+    {
+        numberText.text = "A: " + _value;
+    }
+
+    public void ChangeTextToB(string _value)
+    {
+        numberText.text = "B: " + _value;
     }
 }
